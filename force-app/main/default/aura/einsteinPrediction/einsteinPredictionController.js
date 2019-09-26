708bfca2e0c5a5c170760bc5910f7e12049ab480
Gabe Sumner <gabe.sumner@gmail.com>
2019-09-26 11:30:26 -0700

    Slightly more intelligent Einstein predictions

--- force-app/main/default/aura/einsteinPrediction/einsteinPredictionController.js
+++ force-app/main/default/aura/einsteinPrediction/einsteinPredictionController.js
@@ -1,5 +1,24 @@
 ({
-    myAction : function(component, event, helper) {
+    recordLoaded: function (component, event, helper) {
+        console.log('einsteinPrediction: recordLoaded');
+        component.set('v.model', component.get('v.simpleRecord.Model__c'));
+        component.set('v.battery', component.get('v.simpleRecord.Battery__c'));
 
-    }
+        if (component.get('v.simpleRecord.Model__c') != 'Flash') {
+            $A.util.addClass(component.find("modelPrediction"), "slds-hide");
+        } else {
+            $A.util.removeClass(component.find("modelPrediction"), "slds-hide");
+        }
+
+        if (component.get('v.simpleRecord.Battery__c') == '100 kWh') {
+            $A.util.addClass(component.find("batteryPrediction"), "slds-hide");
+        } else {
+            $A.util.removeClass(component.find("batteryPrediction"), "slds-hide");
+        }
+
+        var changeType = event.getParams().changeType;
+        if (changeType === "CHANGED") {
+            component.find("recordEdit").reloadRecord();
+        }
+    },
 })
