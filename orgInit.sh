32a5686dd55bd2fc1b13cd57545d229be905157b
mshanemc <shane.mclaughlin@salesforce.com>
2019-09-26 13:44:09 -0500

    theme activation

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 7 -s --wait 60 --userprefix wall -o df19.cg
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
-# Activate the Electron Theme
+sfdx shane:theme:activate -n "Electron"
 sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
 # Review and accept the security update
