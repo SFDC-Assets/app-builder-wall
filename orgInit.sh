404595088de1796cf65ab73f0ddf5243af332258
mshanemc <shane.mclaughlin@salesforce.com>
2019-09-26 11:50:35 -0500

    bump api version in project, uncomment orginit file

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,8 @@
 # sfdx force:config:set apiVersion=47.0
-# sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60
-# sfdx force:source:push
-# sfdx force:user:permset:assign -n electron
+sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60
+sfdx force:source:push
+sfdx force:user:permset:assign -n electron
 # Activate the Electron Theme
-# sfdx shane:user:password:set -g User -l User -p salesforce1
-# sfdx force:org:open
+sfdx shane:user:password:set -g User -l User -p salesforce1
+sfdx force:org:open
 # Review and accept the security update
