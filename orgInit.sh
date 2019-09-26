4709c6113f8756b10440ee59a32accce4fba0ae5
mshanemc <shane.mclaughlin@salesforce.com>
2019-09-26 12:23:45 -0500

    flag typo

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 # sfdx force:config:set apiVersion=47.0
-sfdx shane:org:create -f config/project-scratch-def.json -d 7 -s -w 60 --userprefix wall -o df19.cg
+sfdx shane:org:create -f config/project-scratch-def.json -d 7 -s --wait 60 --userprefix wall -o df19.cg
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
 # Activate the Electron Theme
